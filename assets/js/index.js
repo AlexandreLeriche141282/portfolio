ed5352c5d142ce2e4afbf73065429e6f952aa064
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2024-01-24 15:09:48 +0100

    site2024

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,36 @@ const scroll = new LocomotiveScroll({
   tablet: { smooth: true },
   smartphone: { smooth: true },
 });
+// Arrow up
+const faArrowUp = document.querySelector(".fa-arrow-up")
+// let arrowUp = document.querySelector(".arrowUp")
+const arrowUp = document.querySelector('.arrowUp');
+
+// arrowUp.addEventListener('click', () => {
+
+//     window.scrollTo({
+//       Top:0
+        
+//     })
+// console.log(arrowUp);
+// })
 
 // Body color
 scroll.on("scroll", () => {
   if (document.querySelector("#color.is-inview")) {
     document.body.style.background = "#000101";
     document.body.style.color = "#fefeff";
+    faArrowUp.style.borderColor = "white"
+    
+    
+    
   } else {
     document.body.style.background = "#fefeff";
     document.body.style.color = "#000101";
+  faArrowUp.style.borderColor = "black"
+  
+    
   }
 });
+
+
